2ccb2aaf68f02e8b7bac79de898a2acb4db273b7
xFGhoul <dauntejules9@gmail.com>
2024-12-14 21:50:21 -0400

    ♻️ `json` -> `application/json`

--- src/api/users.rs
+++ src/api/users.rs
@@ -19,7 +19,7 @@ use crate::schema::users::dsl::*;
 // Redis
 use fred::prelude::*;
 
-#[get("/get/<user_id>", format = "json")]
+#[get("/get/<user_id>", format = "application/json")]
 pub async fn get(user_id: i32, pool: &State<DatabasePool>, redis: &State<RedisPool>) -> Option<Json<User>> {
     // [?] Check if User Data is in Redis Cache
     let cache: Value = redis.get(user_id.to_string()).await.expect("[-] Getting User In Cache Failed!");
@@ -60,7 +60,7 @@ pub async fn get(user_id: i32, pool: &State<DatabasePool>, redis: &State<RedisPo
     }
 }
 
-#[get("/all", format = "json")]
+#[get("/all", format = "application/json")]
 pub async fn all(pool: &State<DatabasePool>) -> Option<Json<Vec<User>>> {
     // [=] Creating Database Connection
     let connection = &mut pool.get().await.ok()?;
